c7cbf5b5e22f7ebf40b323116436c1e85e4d600b
Filip <filip.milak@pinecone.hr>
2019-10-10 09:05:34 +0200

    listener fix

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const ReactNative = require('react-native');
-const { NativeModules, DeviceEventEmitter } = ReactNative;
+const {NativeModules, DeviceEventEmitter} = ReactNative;
 const HoneywellScanner = NativeModules.HoneywellScanner || {}; // Hacky fallback for iOS
 
-const allowedEvents = [
-  HoneywellScanner.BARCODE_READ_SUCCESS,
-  HoneywellScanner.BARCODE_READ_FAIL,
-];
+var subscriptionBarcodeReadSuccess = null;
+var subscriptionBarcodeReadFail = null;
 
 /**
  * Listen for available events
  * @param  {String} eventName Name of event one of barcodeReadSuccess, barcodeReadFail
  * @param  {Function} handler Event handler
  */
-HoneywellScanner.on = (eventName, handler) => {
-  if (!allowedEvents.includes(eventName)) {
-    throw new Error(`Event name ${eventName} is not a supported event.`);
-  }
-  DeviceEventEmitter.addListener(eventName, handler);
+HoneywellScanner.onBarcodeReadSuccess = handler => {
+  subscriptionBarcodeReadSuccess = DeviceEventEmitter.addListener(HoneywellScanner.BARCODE_READ_SUCCESS, handler);
+};
+
+HoneywellScanner.onBarcodeReadFail = handler => {
+  subscriptionBarcodeReadFail = DeviceEventEmitter.addListener(HoneywellScanner.BARCODE_READ_FAIL, handler);
 };
 
 /**
@@ -24,11 +23,11 @@ HoneywellScanner.on = (eventName, handler) => {
  * @param  {String} eventName Name of event one of barcodeReadSuccess, barcodeReadFail
  * @param  {Function} handler Event handler
  */
-HoneywellScanner.off = (eventName, handler) => {
-  if (!allowedEvents.includes(eventName)) {
-    throw new Error(`Event name ${eventName} is not a supported event.`);
-  }
-  DeviceEventEmitter.removeListener(eventName, handler);
+HoneywellScanner.offBarcodeReadSuccess = () => {
+  subscriptionBarcodeReadSuccess.remove();
+};
+HoneywellScanner.offBarcodeReadFail = () => {
+  subscriptionBarcodeReadFail.remove();
 };
 
 module.exports = HoneywellScanner;
